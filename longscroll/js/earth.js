ffd3b1f1b17e560ddefeed01a822c5fc2a54fed2
Xinchi <38084090+Swilor@users.noreply.github.com>
2018-05-29 21:34:04 -0700

    basic frames

---
+++ longscroll/js/earth.js
@@ -0,0 +1,20 @@
+var earthY=document.getElementById('img_earth');
+
+var i=0;
+
+// for(i==0,i<=10,i=i+1){
+//   if(i>=0 && i<=5){
+//     img_earth.style.top=i*10+"px";
+//   }
+//   if(i>5 && i<10){
+//     img_earth.style.top=i*10-50+"px";
+//   }
+//   if(i===10){
+//     img_earth.style.top=0+"px";
+//     i=0;
+//   }
+// }
+
+window.onscroll = function(){
+  
+}
